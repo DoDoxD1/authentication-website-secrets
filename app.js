28e9167ad11579375d1c49b708528b7c8cf1c1b4
= <=>
2024-07-29 18:57:56 +0530

    Level 1 authentication: checking username pass

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -8,9 +9,50 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+mongoose.connect("mongodb://localhost:27017/userDB");
+const userSchema = mongoose.Schema({
+  email: String,
+  password: String,
+});
+const User = new mongoose.model("User", userSchema);
+
 app.route("/").get((req, res) => {
   res.render("home");
 });
+
+app
+  .route("/register")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post(async (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+    const user = new User({
+      email: email,
+      password: password,
+    });
+    const result = await user.save();
+    if (result) {
+      res.render("secrets");
+    } else console.log("Error!");
+  });
+
+app
+  .route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post(async (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+    const result = await User.findOne({ email: email });
+    if (result) {
+      if (result.password === password) {
+        res.render("secrets");
+      } else res.send("Incorrect Password");
+    } else res.send("User do not exists in the database!");
+  });
 
 app.listen(PORT, () => {
   console.log(`App started at port: ${PORT}`);
